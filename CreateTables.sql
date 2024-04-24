5b000f0b532ab1e8341bbbaa188a7ef74d970699
Amy Pegram <amy.pegram@hotmail.com>
2024-04-24 15:43:50 +0200

    feature: creating album adding function plus new more table info

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,41 +1,46 @@
 USE [master]
-GO
 
-DROP DATABASE TaylorSwiftAlbumRanker
+DROP DATABASE IF EXISTS TaylorSwiftAlbumRanker
 CREATE DATABASE TaylorSwiftAlbumRanker
 
 USE [TaylorSwiftAlbumRanker] 
 
+DROP TABLE IF EXISTS Ranking
+DROP TABLE IF EXISTS Users
+DROP TABLE IF EXISTS Albums
+DROP TABLE IF EXISTS Roles
+DROP TABLE IF EXISTS RolePermissions
+
 CREATE TABLE RolePermissions(
-	RolePermissionID int NOT NULL PRIMARY KEY,
-	PermissionName varchar,
+	RolePermissionID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	PermissionName varchar(150),
 	CanPerform BIT
 )
 
 CREATE TABLE Roles(
-	RoleID int NOT NULL PRIMARY KEY,
-	RoleName varchar,
+	RoleID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	RoleName varchar(150),
 	RolePermissionID int FOREIGN KEY REFERENCES RolePermissions(RolePermissionID)
 )
 
+
 CREATE TABLE Albums(
-	AlbumID int NOT NULL PRIMARY KEY,
-	AlbumName varchar,
-	PictureHyperlinks varchar 
+	AlbumID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	AlbumName varchar(150),
+	PictureHyperlink varchar(150) 
 )
 
 
 CREATE TABLE Users(
-	UserID int NOT NULL PRIMARY KEY,
-	Username varchar,
+	UserID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Username varchar(150),
 	RoleID int FOREIGN KEY REFERENCES Roles(RoleID)
 )
 
+
 CREATE TABLE Ranking(
-	RankingID int NOT NULL PRIMARY KEY,
+	RankingID int NOT NULL PRIMARY KEY IDENTITY(1,1),
 	AlbumID int FOREIGN KEY REFERENCES Albums(AlbumID),
 	Ranking int,
 	UserID int FOREIGN KEY REFERENCES Users(UserID)
 )
-
-MERGE INTO Albums
